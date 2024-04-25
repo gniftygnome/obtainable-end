9d819e6586ec283b75ca18eaf70d125f2568aab5
gniftygnome <minecraft@gnifty.net>
2024-04-25 01:16:54 -0700

    Release for Minecraft 1.20.5.
    - Release for Minecraft 1.20.5
    * Switch the tag modifier to use ModifyReturn

--- src/main/java/net/gnomecraft/obtainableend/mixin/MixinTagManagerLoader.java
+++ src/main/java/net/gnomecraft/obtainableend/mixin/MixinTagManagerLoader.java
@@ -1,28 +1,24 @@
 package net.gnomecraft.obtainableend.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.gnomecraft.obtainableend.tags.TagsLoadedEvent;
 import net.minecraft.registry.tag.TagManagerLoader;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourceReloader;
-import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 @Mixin(TagManagerLoader.class)
 public class MixinTagManagerLoader {
     @Shadow
     private List<TagManagerLoader.RegistryTags<?>> registryTags;
 
-    @Inject(method="reload", at=@At("TAIL"), cancellable = true)
-    private void obtainableend$reloadAdder(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
-        cir.setReturnValue(cir.getReturnValue().whenComplete(this::obtainableend$whenComplete));
+    @ModifyReturnValue(method="reload", at=@At("RETURN"))
+    @SuppressWarnings("unused")
+    private CompletableFuture<Void> obtainableend$reloadAdder(CompletableFuture<Void> original) {
+        return original.whenComplete(this::obtainableend$whenComplete);
     }
 
     private void obtainableend$whenComplete(Object object, Throwable throwable) {
